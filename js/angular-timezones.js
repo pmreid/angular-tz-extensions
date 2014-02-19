d81102b71f3536c7b17465b538222548dc1d6a7a
Chris Houseknecht <chouse@ansible.com>
2014-02-19 03:09:27 -0500

    Added jstz package. Added getZonesList method to build selection list of available timezones. Example to include new method.

--- js/angular-timezones.js
+++ js/angular-timezones.js
@@ -1,194 +1,247 @@
+/* angular-timezones.js 
+ *
+ * Copyright 2014 Chris Houseknecht
+ * Originally forked from https://github.com/michaelahlers/angular-timezones
+ * Distributed freely under terms of the MIT License (MIT)
+ *
+ * Creates Timezones module
+ *
+ */
+
 (function (root) {
 
-  var angular = root.angular
-    , timezoneJS = root.timezoneJS
-    , jstz = root.jstz
+    var angular = root.angular,
+        timezoneJS = root.timezoneJS,
+        jstz = root.jstz,
 
-  var toExtendedNative = function (wrapped) {
-    /* Tricks the isDate method in Angular into treating these objects like it
-     * would any other Date. May be horribly slow. */
-    var native = new Date()
-    for (key in wrapped) {
-      native[key] = wrapped[key]
-    }
-    return native
-  }
-
-  var module = angular.module('Timezones', [])
-
-  module.config(function () {
-    timezoneJS.fromLocalString = function (str, tz) {
-      // https://github.com/csnover/js-iso8601/blob/master/iso8601.js – MIT license
-
-      var minutesOffset = 0
-      var struct = /^(\d{4}|[+\-]\d{6})(?:-(\d{2})(?:-(\d{2}))?)?(?:T(\d{1,2}):(\d{1,2})(?::(\d{1,2})(?:\.(\d{3}))?)?(?:(Z)|([+\-])(\d{2})(?::(\d{2}))?)?)?$/.exec(str)
-      var numericKeys = [ 1, 4, 5, 6, 7, 10, 11 ]
-      // avoid NaN timestamps caused by “undefined” values being passed to Date.UTC
-      for (var i = 0, k; (k = numericKeys[i]); ++i) {
-        struct[k] = +struct[k] || 0
-      }
-
-      // allow undefined days and months
-      struct[2] = (+struct[2] || 1) - 1
-      struct[3] = +struct[3] || 1
-
-      return toExtendedNative(new timezoneJS.Date(struct[1], struct[2], struct[3], struct[4], struct[5], struct[6], struct[7], tz))
-    }
-  })
-
-  module.constant('$timezones.definitions.location', '/tz/data')
-
-  module.run(['$timezones.definitions.location', '$log', function (location, $log) {
-    timezoneJS.timezone.zoneFileBasePath = location
-    timezoneJS.timezone.init({ async : false })
-  }])
-
-  module.factory('$timezones', function () {
-    var resolve = function (timezone, reference) {
-      if ('number' === typeof(reference)) {
-        reference = new Date(reference)
-      }
-
-      /*
-       * TODO: Support resolution without reference dates.
-       *
-       * For now, we must use reference dates. There's just not enough time
-       * to write and test code that would resolve all possible definitions
-       * for any given timezone. Hopefully the TimezoneJS folks will support
-       * that some day.
-       */
-
-      if (!angular.isDate(reference)) {
-        throw {
-          name : 'DateObjectExpected',
-          message : 'Expected a Date object; got "' + date + '".'
+    toExtendedNative = function (wrapped) {
+        /* Tricks the isDate method in Angular into treating these objects like it
+         * would any other Date. May be horribly slow. */
+        var key, native = new Date();
+        for (key in wrapped) {
+            native[key] = wrapped[key];
         }
-      }
+        return native;
+    },
 
-      /* This is not terribly efficient, but necessary because some timezone
-       * specifics (like the abbreviation and offset) are temporal. */
-      reference = new timezoneJS.Date(reference, timezone)
+    module = angular.module('Timezones', []);
 
-      var name = reference.getTimezone()
+    module.config(function () {
+        timezoneJS.fromLocalString = function (str, tz) {
+            // https://github.com/csnover/js-iso8601/blob/master/iso8601.js – MIT license
 
-      var result = {
-        name : name,
-        abbreviation : reference.getTimezoneAbbreviation(),
-        offset : reference.getTimezoneOffset(),
-        region : name.split('/')[0],
-        locality : name.split('/')[1].replace('_', ' ')
-      }
+            //var minutesOffset = 0,
+            var struct = /^(\d{4}|[+\-]\d{6})(?:-(\d{2})(?:-(\d{2}))?)?(?:T(\d{1,2}):(\d{1,2})(?::(\d{1,2})(?:\.(\d{3}))?)?(?:(Z)|([+\-])(\d{2})(?::(\d{2}))?)?)?$/.exec(str),
+                numericKeys = [1, 4, 5, 6, 7, 10, 11],
+                i, k;
+            // avoid NaN timestamps caused by “undefined” values being passed to Date.UTC
+            for (i = 0;(k = numericKeys[i]); ++i) {
+                struct[k] = +struct[k] || 0;
+            }
 
-      return result
-    }
+            // allow undefined days and months
+            struct[2] = (+struct[2] || 1) - 1;
+            struct[3] = +struct[3] || 1;
 
-    return {
+            return toExtendedNative(new timezoneJS.Date(struct[1], struct[2], struct[3], struct[4], struct[5], struct[6], struct[7], tz));
+        };
+    });
 
-      /**
-       * Aligns the provided Date object to the specified timezone.
-       *
-       * @param date
-       * Reference date.
-       *
-       * @param timezone
-       * An Olson name (e.g., America/New_York), or a timezone object
-       * (produced by the resolve function).
-       *
-       * @returns {*} A Date "aligned" to the desired timezone.
-       */
-      align : function (date, timezone, silent) {
-        if (!angular.isDate(date)) {
-          throw {
-            name : 'DateObjectExpected',
-            message : 'Expected a Date object; got "' + date + '".'
-          }
+    module.constant('$timezones.definitions.location', '/tz/data');
+
+    module.run(['$timezones.definitions.location',
+        function (location) {
+            timezoneJS.timezone.zoneFileBasePath = location;
+            timezoneJS.timezone.init({ async: false });
         }
+    ]);
 
-        if (angular.isObject(timezone) && timezone.name) {
-          return toExtendedNative(new timezoneJS.Date(date, timezone.name))
-        }
+    module.factory('$timezones',['$log', '$http', '$timezones.definitions.location', function ($log, $http, location) {
+        
+        var resolve = function (timezone, reference) {
+            var name, result;
 
-        if (angular.isString(timezone)) {
-          return toExtendedNative(new timezoneJS.Date(date, timezone))
-        }
+            if ('number' === typeof (reference)) {
+                reference = new Date(reference);
+            }
 
-        if (true === silent) {
-          return date
-        }
+            /*
+             * TODO: Support resolution without reference dates.
+             *
+             * For now, we must use reference dates. There's just not enough time
+             * to write and test code that would resolve all possible definitions
+             * for any given timezone. Hopefully the TimezoneJS folks will support
+             * that some day.
+             */
 
-        throw new Error('The timezone argument must either be an Olson name (e.g., America/New_York), or a timezone object (produced by the resolve function) bearing an Olson name on the name property.')
-      },
+            if (!angular.isDate(reference)) {
+                $log.error('DateObjectExpected. Expected a Date object; got: ' + typeof reference + ' of ' + reference);
+                throw {
+                    name: 'DateObjectExpected',
+                    message: 'Expected a Date object; got "' + reference + '".'
+                };
+            }
 
-      /**
-       * Generate an object that defines the timezone.
-       *
-       * @param timezone
-       * An Olson name (e.g., America/New_York) to resolve.
-       *
-       * @param reference
-       * A reference date used to determine values for temporal timezone
-       * properties like the offset and abbreviation (which vary between
-       * standard and daylight times).
-       *
-       * @returns {{name: string, abbreviation: string, offset: number, region: string, locality: string}}
-       */
-      resolve : resolve,
+            /* This is not terribly efficient, but necessary because some timezone
+             * specifics (like the abbreviation and offset) are temporal. */
+            reference = new timezoneJS.Date(reference, timezone);
 
-      /**
-       * If the jsTimezoneDetect library is available, use it to make an
-       * approximate guess at the current timezone Olson name. From there,
-       * perform resolution as usual (which implicitly gets the authoritative
-       * definitions from the IANA database).
-       */
-      getLocal : function () {
-        if ('undefined' === typeof(jstz) || 'function' !== typeof(jstz.determine)) {
-          throw {
-            name : 'JSTZLibraryMissing',
-            message : 'The jsTimezoneDetect library, available at https://bitbucket.org/pellepim/jstimezonedetect, is required to detect the local timezone.'
-          }
-        }
+            name = reference.getTimezone();
 
-        var name = jstz.determine().name()
-          , now = new Date()
+            result = {
+                name: name,
+                abbreviation: reference.getTimezoneAbbreviation(),
+                offset: reference.getTimezoneOffset(),
+                region: name.split('/')[0],
+                locality: name.split('/')[1].replace('_', ' ')
+            };
 
-        return resolve(name, now)
-      }
+            return result;
+        };
 
-    }
-  })
+        return {
 
-  module.filter('tzAlign', function ($timezones) {
-    return function (date, timezone) {
-      if (!(angular.isDate(date) || angular.isNumber(date) || angular.isString(date)) || !(angular.isString(timezone) || angular.isObject(timezone))) {
-        return date
-      }
+            /**
+             * Aligns the provided Date object to the specified timezone.
+             *
+             * @param date
+             * Reference date.
+             *
+             * @param timezone
+             * An Olson name (e.g., America/New_York), or a timezone object
+             * (produced by the resolve function).
+             *
+             * @returns {*} A Date "aligned" to the desired timezone.
+             */
+            align: function (date, timezone, silent) {
+                var eMsg;
 
-      var verifiedDate = date
+                if (!angular.isDate(date)) {
+                    eMsg = 'Expected a Date object; got: ' + typeof reference + ' of ' + date;
+                    $log.error(eMsg);
+                    throw {
+                        name: 'DateObjectExpected',
+                        message: eMsg
+                    };
+                }
 
-      if (angular.isNumber(date)) {
-        verifiedDate = new Date(date)
-      } else if (angular.isString(date)) {
-        var milliseconds = parseInt(date)
+                if (angular.isObject(timezone) && timezone.name) {
+                    return toExtendedNative(new timezoneJS.Date(date, timezone.name));
+                }
 
-        if (!angular.isNumber(milliseconds)) {
-          milliseconds = Date.parse(date)
-        }
+                if (angular.isString(timezone)) {
+                    return toExtendedNative(new timezoneJS.Date(date, timezone));
+                }
 
-        verifiedDate = new Date(milliseconds)
-      }
+                if (true === silent) {
+                    return date;
+                }
+                eMsg = 'The timezone argument must either be an Olson name (e.g., America/New_York), or a timezone object (produced by the resolve function) bearing an Olson name on the name property.';
+                $log.error(eMsg);
+                throw new Error(eMsg);
+            },
 
-      if (!angular.isDate(verifiedDate) || isNaN(verifiedDate.getTime())) {
-        return date
-      }
+            /**
+             * Generate an object that defines the timezone.
+             *
+             * @param timezone
+             * An Olson name (e.g., America/New_York) to resolve.
+             *
+             * @param reference
+             * A reference date used to determine values for temporal timezone
+             * properties like the offset and abbreviation (which vary between
+             * standard and daylight times).
+             *
+             * @returns {{name: string, abbreviation: string, offset: number, region: string, locality: string}}
+             */
+            resolve: resolve,
 
-      var alignedDate = $timezones.align(verifiedDate, timezone, true)
+            /**
+             * If the jsTimezoneDetect library is available, use it to make an
+             * approximate guess at the current timezone Olson name. From there,
+             * perform resolution as usual (which implicitly gets the authoritative
+             * definitions from the IANA database).
+             */
+            getLocal: function () {
+                var eMsg, name, now;
 
-      if (isNaN(alignedDate.getTime())) {
-        return date
-      }
+                if ('undefined' === typeof (jstz) || 'function' !== typeof (jstz.determine)) {
+                    eMsg = 'The jsTimezoneDetect library, available at https://bitbucket.org/pellepim/jstimezonedetect, is required to detect the local timezone.';
+                    $log.error(eMsg);
+                    throw {
+                        name: 'JSTZLibraryMissing',
+                        message: eMsg
+                    };
+                }
 
-      return alignedDate
-    }
-  })
+                name = jstz.determine().name();
+                now = new Date();
+                return resolve(name, now);
+            },
 
-})(this)
+            getZoneList: function(scope) {
+                var zones = [], sorted;
+                if (localStorage.zones) {
+                    scope.$emit('zonesReady');
+                }
+                else {
+                    $http({ method: 'GET', url: location + '/zone.tab'})
+                        .success(function(data) {
+                            var i, fields, lines = data.match(/[^\r\n]+/g);
+                            for (i=0; i < lines.length; i++) {
+                                if (!/^#/.test(lines[i])) {
+                                    fields = lines[i].split(/\s+/);
+                                    zones.push(fields[2]);
+                                }
+                            }
+                            sorted = zones.sort();
+                            zones = [];
+                            for (i=0; i < sorted.length; i++) {
+                                zones.push({ name: sorted[i] });
+                            }
+                            localStorage.zones = JSON.stringify(zones);
+                            scope.$emit('zonesReady');
+                        })
+                        .error(function() {
+                            $log.error('Failed to load ' + location + '/zone.tab');
+                        });
+                }
+            }
+        };
+    }]);
+
+    module.filter('tzAlign', ['$timezones', function ($timezones) {
+        return function (date, timezone) {
+            if (!(angular.isDate(date) || angular.isNumber(date) || angular.isString(date)) || !(angular.isString(timezone) || angular.isObject(timezone))) {
+                return date;
+            }
+
+            var alignedDate, milliseconds, verifiedDate = date;
+
+            if (angular.isNumber(date)) {
+                verifiedDate = new Date(date);
+            } else if (angular.isString(date)) {
+                milliseconds = parseInt(date);
+
+                if (!angular.isNumber(milliseconds)) {
+                    milliseconds = Date.parse(date);
+                }
+
+                verifiedDate = new Date(milliseconds);
+            }
+
+            if (!angular.isDate(verifiedDate) || isNaN(verifiedDate.getTime())) {
+                return date;
+            }
+
+            alignedDate = $timezones.align(verifiedDate, timezone, true);
+
+            if (isNaN(alignedDate.getTime())) {
+                return date;
+            }
+
+            return alignedDate;
+        };
+    }]);
+
+})(this);
